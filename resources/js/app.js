3e255b57fe73da1d10e6d4c712ea3807b83c9f79
Sergey Golubev <skyandy2796@yandex.ru>
2022-11-24 11:37:19 +0300

    Сделал чтобы пакеты стилей устанавливались через npm

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,6 @@
 import './bootstrap';
+import jQuery from 'jquery';
+import '@fortawesome/fontawesome-free/css/all.css';
+import 'flag-icon-css/css/flag-icons.css';
+
+window.$ = jQuery;
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,8 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Edica :: Home</title>
-    <link rel="stylesheet" href="{{ asset('assets/vendors/flag-icon-css/css/flag-icon.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/vendors/font-awesome/css/all.min.css') }}">
+    @vite(['resources/js/app.js', 'resources/css/css.js'])
+    <script src="{{ asset('js/app.js') }}"></script>
+
     <link rel="stylesheet" href="{{ asset('assets/vendors/aos/aos.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
     <script src="{{ asset('assets/vendors/jquery/jquery.min.js') }}"></script>
@@ -125,8 +126,8 @@
         </div>
     </div>
 </footer>
+
 <script src="{{ asset('assets/vendors/popper.js/popper.min.js') }}"></script>
-<script src="{{ asset('assets/vendors/bootstrap/dist/js/bootstrap.min.js') }}"></script>
 <script src="{{ asset('assets/vendors/aos/aos.js') }}"></script>
 <script src="{{ asset('assets/js/main.js') }}"></script>
 <script>
@@ -134,6 +135,7 @@
         duration: 1000
     });
 </script>
+
 </body>
 
 </html>
