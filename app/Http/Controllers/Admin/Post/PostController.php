b87cb7a578aee3d93501dbbc8064faa20d13818f
Sergey Golubev <skyandy2796@yandex.ru>
2022-12-06 13:26:51 +0300

    added a text editor

---
+++ app/Http/Controllers/Admin/Post/PostController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Post;
+
+use App\Http\Controllers\Controller;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index(){
+        $data = Post::all();
+        return view('admin.posts.index', ['posts' => $data]);
+    }
+}
